17abdf2bddbd80fe6fa01274a1de068eb5fcd02e
tayutaedomo <tayutaedomo@gmail.com>
2015-11-14 21:36:32 +0900

    Small refactoring

--- routes/novels/top/recommend/post.js
+++ routes/novels/top/recommend/post.js
@@ -1,5 +1,6 @@
 var client = require('cheerio-httpcli');
 var models = require('scraping/models');
+var html = require('scraping/utils/html');
 var beautify = require('js-beautify').js_beautify;
 var debug = require('debug')('scraping:routes/novels/top/recommend/post');
 
@@ -13,7 +14,7 @@ module.exports = function(req, res, next) {
     client.fetch(url)
       .then(function (result) {
         result.$('.recommend_novel').each(function(index, element){
-          recommends.push(extractRecommendData(result.$, element));
+          recommends.push(html.extractRecommend(result.$, element));
         });
       })
       .catch(function (err) {
@@ -39,40 +40,4 @@ module.exports = function(req, res, next) {
     res.render('novels/top/recommend', { data: { params: req.body } });
   }
 };
-
-function extractRecommendData($, element) {
-  var data = {};
-
-  var matches = $(element).find('a').attr('href').match(/\/(n[0-9a-z]+)\//);
-  data['ncode'] = matches[1];
-
-  data['url'] = $(element).find('a').attr('href');
-  data['title'] = $(element).find('a .reconovel_title').text();
-
-  $(element).find('li').each(function(index, element){
-    switch (index) {
-      case 0:
-        var matches = $(element).text().match(/.+/);
-        data['category'] = matches[0];
-        break;
-
-      case 1:
-        var matches = $(element).text().match(/(.+)\(全(\d+)部\)/);
-        data['state'] = matches[1];
-        data['pageCount'] = matches[2];
-        break;
-
-      case 2:
-        var matches = $(element).text().match(/(\d+) user/);
-        data['bookmarkCount'] = matches[1];
-        break;
-
-      case 3:
-        var matches = $(element).text().match(/最終掲載日：(.+)/)
-        data['lastPublishedAt'] = matches[1];
-    }
-  });
-
-  return data;
-}
 
